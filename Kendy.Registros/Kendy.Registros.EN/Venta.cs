77aa9ffd88023c78aceaefbb924b433c99be2bc6
Gabrielinares <68619397+Gabrielinares@users.noreply.github.com>
2020-07-21 16:47:38 -0600

    Add files via upload

---
+++ Kendy.Registros/Kendy.Registros.EN/Venta.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kendy.Registros.EN
+{
+    public class Venta
+    {
+        public int Id { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        public float Total { get; set; }
+
+        public int EmpleadoId { get; set; }
+
+        public int ClienteId { get; set; }
+
+        public Venta () { } //Constructor vacio
+
+        public Venta (int pId, DateTime pFecha, float pTotal, int pEmpleadoId, int pCLienteId)
+        {
+            Id = pId;
+
+        }
+
+    }
+}
