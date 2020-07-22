b1a659f04b3a94e3c8f0acfbc532544c812e5397
MoiGuzman <68622703+MoiGuzman@users.noreply.github.com>
2020-07-21 21:42:59 -0600

    Add files via upload

---
+++ Kendy.Registros/Kendy.Registros.EN/Existencias.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kendy.Registros.EN
+{
+    public class Existencias
+    {
+        public int Id { get; set; }
+        public int ProductoId { get; set; }
+        public string Tipo { get; set; }
+        public DateTime Fecha { get; set; }
+        public Existencias () { }
+        public Existencias(int pId, int pProductorId, string pTipo, DateTime pFecha)
+        {
+            Id = pId;
+            ProductoId = pProductorId;
+            Tipo = pTipo;
+            Fecha = pFecha;
+        }
+    }
+}
